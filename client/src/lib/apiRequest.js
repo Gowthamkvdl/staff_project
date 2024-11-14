bdc19c521b1444be4c8b591059d55b52681e699c
gowthamkvdl <gowthamkvdl@gmail.com>
2024-11-14 18:34:25 +0530

    guigiuh

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const apiRequest = axios.create({
   baseURL: "https://staffprojectbackend.onrender.com/api",
   withCredentials: true,
-});   
+});  
+ 
 // const apiRequest = axios.create({
 //   baseURL: "http://localhost:8080/api",
 //   withCredentials: true,
